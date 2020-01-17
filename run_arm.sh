a73723be8393bce0b7ceb0004881045b4840fce0
yangzhou <yangzhou@node-0.yangzhou-qv63200.lambda-mpi-pg0.clemson.cloudlab.us>
2020-01-17 00:10:10 -0500

    mem: enable different freq and multi-program
    Change-Id: Ibb93ddc45cf980eb5c16341bc90698a4dc81f50c

--- run_arm.sh
+++ run_arm.sh
@@ -3,9 +3,17 @@
 #     --redirects /lib=/usr/aarch64-linux-gnu/lib \
 #     -c ./test/testfs
 
-./build/ARM/gem5.opt ./configs/example/se.py \
+./build/ARM/gem5.opt ./configs/example/se_nic.py \
     --interp-dir /usr/aarch64-linux-gnu \
     --redirects /lib=/usr/aarch64-linux-gnu/lib \
-    -c ~/NetBricks-GEM5/target/aarch64-unknown-linux-gnu/debug/spmc \
-    -n 33 \
-    --mem-size=10240000000
+    -c "/users/yangzhou/NetBricks-GEM5/target/aarch64-unknown-linux-gnu/debug/spmc;/users/yangzhou/NetBricks-GEM5/target/aarch64-unknown-linux-gnu/debug/macswap" \
+    -n 3 \
+    --cpu-type=TimingSimpleCPU \
+    --cacheline_size=128 \
+    --caches \
+    --l1d_size=32kB --l1d_assoc=32 \
+    --l1i_size=78kB --l1i_assoc=39 \
+    --l2cache \
+    --l2_size=4MB --l2_assoc=16 \
+    --mem-size=16GB --mem-type=DDR3_1600_8x8 \
+    --asic-clock=10GHz \
